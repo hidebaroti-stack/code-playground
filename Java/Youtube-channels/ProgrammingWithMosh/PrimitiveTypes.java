a0ae92332900b0079265e639a9adafbdd8fca459
Debaroti Deb Nath <hidebaroti@gmail.com>
2026-06-04 17:21:18 +0600

    PrimitiveTypes.java, Done.

---
+++ Java/Youtube-channels/ProgrammingWithMosh/PrimitiveTypes.java
@@ -0,0 +1,20 @@
+public class PrimitiveTypesDemo {
+    public static void main(String[] args) {
+
+        int myAge = 30;
+        int herAge = myAge;
+        byte age = 12;
+        int viewscount = 123_456_789;
+        long viewsCount = 3_123_456_789L;
+        float price = 10.89F;
+        boolean isEligible = true;
+
+        System.out.println(herAge);
+        System.out.println(age);
+        System.out.println(viewscount);
+        System.out.println(viewsCount);
+        System.out.println(price);
+        System.out.println(isEligible);
+
+    }
+}
