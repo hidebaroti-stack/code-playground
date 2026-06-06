065bd1b57b893557001901fae7f24b805c0f1cb0
Debaroti Deb Nath <hidebaroti@gmail.com>
2026-06-06 19:46:28 +0600

    Data.java, Done.

---
+++ Java/Youtube-channels/ProgrammingWithMosh/Date.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Date {
+
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Current time: " + now);
+    }
+}
