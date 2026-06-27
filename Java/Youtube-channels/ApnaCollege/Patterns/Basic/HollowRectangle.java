0faf3cbf72446f53c32b0590c778aa3de9b0483c
Debaroti Deb Nath <hidebaroti@gmail.com>
2026-06-27 21:20:21 +0600

    HollowRectangle.java, Done.

---
+++ Java/Youtube-channels/ApnaCollege/Patterns/Basic/HollowRectangle.java
@@ -0,0 +1,26 @@
+package Patterns.Basic;
+
+import java.util.Scanner;
+
+public class HollowRectangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the rows: ");
+        int n = scanner.nextInt();
+
+        System.out.print("Enter the columns: ");
+        int m = scanner.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (i == 1 || j == 1 || i == n || j == m) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
