f20365a2a5da07fd0f13551f85b51ee355031621
Debaroti Deb Nath <hidebaroti@gmail.com>
2026-06-27 21:21:48 +0600

    FloydTriangle.java, Done.

---
+++ Java/Youtube-channels/ApnaCollege/Patterns/Basic/FloydTriangle.java
@@ -0,0 +1,22 @@
+package Patterns.Basic;
+
+import java.util.Scanner;
+
+public class FloydTriangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the rows: ");
+        int n = scanner.nextInt();
+
+        int number = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(number + " ");
+                number++;
+            }
+            System.out.println();
+        }
+    }
+}
