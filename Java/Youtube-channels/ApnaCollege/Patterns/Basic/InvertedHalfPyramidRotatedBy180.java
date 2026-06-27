ec74d4668fb5f2fe337034feedd14c4316a52912
Debaroti Deb Nath <hidebaroti@gmail.com>
2026-06-27 21:23:11 +0600

    InvertedHalfPyramidRotatedBy180.java, Done.

---
+++ Java/Youtube-channels/ApnaCollege/Patterns/Basic/InvertedHalfPyramidRotatedBy180.java
@@ -0,0 +1,22 @@
+package Patterns.Basic;
+
+import java.util.Scanner;
+
+public class InvertedHalfPyramidRotatedBy180 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the rows: ");
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
