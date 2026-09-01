a250925ef966ec580ef8440855e3f7e266f8ed4f
Debaroti Deb Nath <hidebaroti@gmail.com>
2026-09-01 22:05:58 +0600

    Lab 1 Task 1: Integer input

---
+++ C-programming/CSE103SAJEEB/LAB1/Task1_Integer_Input.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// Task 1: Write a C program that takes an integer input from the user and prints it back.
+int main()
+{
+    int i;
+    printf("Enter an integer: ");
+    scanf("%d", &i);
+    printf("You entered: %d\n", i);
+    return 0;
+}
