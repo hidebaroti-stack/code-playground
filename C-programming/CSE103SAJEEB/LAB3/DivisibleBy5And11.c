ef7ea6d9ddb9abdecfc98ffa2b9d2face40671df
hidebaroti-stack <hidebaroti@gmail.com>
2026-09-06 23:36:48 +0600

    LAB3 Task: Divisible by 5 and 11

---
+++ C-programming/CSE103SAJEEB/LAB3/DivisibleBy5And11.c
@@ -0,0 +1,17 @@
+/*Write a C program to check whether a number is divisible by 5 and 11 or
+not.*/
+
+#include <stdio.h>
+
+int main()
+{
+    int num;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    if (num % 5 == 0 && num % 11 == 0)
+        printf("Divisible.");
+    else
+        printf("Not.");
+}
