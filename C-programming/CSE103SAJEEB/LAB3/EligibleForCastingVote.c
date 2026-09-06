ab84541fba43c5f47b7ae80b95b8dcf0804228c5
hidebaroti-stack <hidebaroti@gmail.com>
2026-09-06 23:37:07 +0600

    LAB3 Task: Voting eligibility

---
+++ C-programming/CSE103SAJEEB/LAB3/EligibleForCastingVote.c
@@ -0,0 +1,17 @@
+/*Write a C program to read the age of a candidate and determine whether it
+is eligible for casting his/her own vote.*/
+
+#include <stdio.h>
+
+int main()
+{
+    int age;
+
+    printf("Enter a age: ");
+    scanf("%d", &age);
+
+    if (age >= 18)
+        printf("Eligible for vote.");
+    else
+        printf("Not eligible.");
+}
